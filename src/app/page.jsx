975c5b13504322cda9aaea98b952bb1e949a98e6
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2024-04-19 08:51:02 -0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,14 +19,26 @@ export default function Home() {
         <div className="flex flex-col h-full px-4 lg:px-28 2xl:px-36">
 
           <div className="flex w-full h-full justify-center">
-            <div className="flex flex-row gap-4 h-3/4 w-[100rem] sm:w-[80rem] h-full justify-center">
-                <div className="invisible md:visible md:w-1/4 h-full border">a</div>
+            <div className="flex flex-row gap-4 h-4/5 w-[100rem] sm:w-[80rem] h-full justify-center">
+                
+                <div className="invisible md:visible md:w-1/4 h-full border border-primary rounded-sm">
+                  <div className="h-full m-6 border border-primary rounded-sm">
+                    
+                  </div>
+                </div>
+
                 <div className="w-full sm:w-3/5 h-3/5 md:h-full bg-gradient-to-b from-light-purple-200 from-15% to-light-purple-300 to-80%"></div>
-                <div className="invisible md:visible md:w-1/4 h-full border">a</div>
+
+              <div className="invisible md:visible md:w-1/4 h-full border border-primary rounded-sm">
+                <div className="h-full m-6 border border-primary rounded-sm">
+                  
+                </div>
+              </div>
+
             </div>
           </div>
 
-          <div className="h-2/5 w-full h-full bg-bg-color"></div>
+          <div className="h-1/5 w-full h-full bg-bg-color"></div>
         </div>
       
       </div>
