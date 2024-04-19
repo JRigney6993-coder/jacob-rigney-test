f83b40626c19bdd3a284f94238c4fc4391b91c63
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2024-04-19 10:26:14 -0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,47 +1,79 @@
 import Image from "next/image";
-
+import { BackgroundBeams } from "@/components/ui/background-beams"
 
 export default function Home() {
   return (
 
     <>
+      <BackgroundBeams className="z-0" />
+
       <div className="relative w-full h-full">
+        <div className="relative w-[100vw] h-[100vh]">
 
-        <div className="invisible sm:visible md:invisible lg:visible absolute left-0 w-[6rem] 2xl:w-[8rem] 3xl:w-[12rem] h-full bg-four-color-gradient">
-          <div className="pt-56 bg-background-color"></div>
-          
-        </div>
+          <div className="z-10 w-full h-full">
+            <div className="flex flex-col justify-between invisible sm:visible md:invisible lg:visible absolute bottom-0 w-[6rem] 2xl:w-[8rem] 3xl:w-[12rem] h-[60vh] bg-four-color-gradient mb-56">
+              
 
-        <div className="invisible sm:visible md:invisible lg:visible absolute right-0 w-[6rem] 2xl:w-[8rem] 3xl:w-[12rem] h-full bg-four-color-gradient">
-          <div className="pt-56 bg-background-color"></div>
-        </div>
+            </div>
 
-        <div className="flex flex-col h-full px-4 lg:px-28 2xl:px-36">
+            <div className="flex flex-col justify-between invisible sm:visible right-0 md:invisible lg:visible absolute bottom-0 w-[6rem] 2xl:w-[8rem] 3xl:w-[12rem] h-[60vh] bg-four-color-gradient mb-56">
 
-          <div className="flex w-full h-full justify-center">
-            <div className="flex flex-row gap-4 h-4/5 w-[100rem] sm:w-[80rem] h-full justify-center">
-                
-                <div className="invisible md:visible md:w-1/4 h-full border border-primary rounded-sm">
-                  <div className="h-full m-6 border border-primary rounded-sm">
-                    
+
+            </div>
+
+            <div className="flex flex-col h-full px-4 lg:px-28 2xl:px-36">
+
+              <div className="flex w-full h-2/3 justify-center">
+                <div className="flex flex-row gap-4 h-full w-[100rem] sm:w-[80rem] h-full justify-center">
+
+                  <div className="invisible md:visible md:w-1/4 h-full border border-primary rounded-sm">
+                    <div className="h-full m-6 border border-primary rounded-sm">
+
+                    </div>
                   </div>
-                </div>
 
-                <div className="w-full sm:w-3/5 h-3/5 md:h-full bg-gradient-to-b from-light-purple-200 from-15% to-light-purple-300 to-80%"></div>
+                  <div className="w-full sm:w-3/5 h-3/5 md:h-full bg-gradient-to-b from-light-purple-200 from-15% to-light-purple-300 to-80%">
+                    現実
+                  </div>
 
-              <div className="invisible md:visible md:w-1/4 h-full border border-primary rounded-sm">
-                <div className="h-full m-6 border border-primary rounded-sm">
-                  
+                  <div className="invisible md:visible md:w-1/4 h-full border border-primary rounded-sm">
+                    <div className="h-full m-6 border border-primary rounded-sm">
+
+                    </div>
+                  </div>
+
                 </div>
               </div>
 
+              <div className="h-1/3 w-full bg-bg-color"></div>
             </div>
           </div>
-
-          <div className="h-1/5 w-full h-full bg-bg-color"></div>
         </div>
-      
       </div>
+      <br />    <br />
+      <br />
+    <br />
+      <br />
+    <br />
+    <br />
+    <br />
+      <br />
+    <br />
+      <br />
+    <br />
+    <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    <br />
+    <br />
+      <br />
+
     </>
 
 
